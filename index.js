166a0d4747620aadab8cc50f9f1278e18386b59c
Nullish <richerde43@gmail.com>
2023-12-28 00:07:33 +0000

    Build general config properties using command line overrides

--- index.js
+++ index.js
@@ -7,7 +7,35 @@ const yargs = require('yargs');
 
 // Load config file
 const paramConfig = process.argv[2];
-const strConfig  = paramConfig.match(/\.\//) ? paramConfig : './' + paramConfig;
+const strConfig = paramConfig.match(/\.\//) ? paramConfig : './' + paramConfig;
 const config = require(strConfig);
 
 console.log(config);
+
+// Get overrides from command line
+const argv = yargs
+    .option('instances', {
+        describe: 'Number of parallel instances',
+        type: 'number'
+    })
+    .option('input', {
+        describe: 'File path to JSON list of URLs to scrape',
+        type: 'string'
+    })
+    .option('output', {
+        describe: 'File path to CSV of output from script',
+        type: 'string'
+    })
+    .option('sitemap', {
+        describe: 'URL of sitemap to use as input ',
+        type: 'string'
+    })
+    .argv;
+
+// Set properties, preferring command line params over config file where supplied.
+const instances = argv.instances || config.instances;
+const input = argv.input || config.input;
+const output = argv.output || config.output;
+const sitemap = argv.sitemap || config.sitemap;
+
+console.log(instances, input, output, sitemap);
