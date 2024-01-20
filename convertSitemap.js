5aa2842b16605e9656973f71147d60e56c7b5e0b
Nullish <richerde43@gmail.com>
2024-01-20 14:43:24 +0000

    #14 WIP: sitemap parsing works for getElement. TODO conditionally delete input

---
+++ convertSitemap.js
@@ -0,0 +1,39 @@
+const { rejects } = require('assert');
+const fs = require('fs');
+const { resolve } = require('path');
+const parser = require('xml2json');
+
+const convertSitemap = async () => {
+	/**
+	 * @function Convert sitemamp
+	 * Convert XML sitemap file to JSON 
+	 */
+
+	const dataStream = "./input/sitemap.xml";
+	const outputPath = "./input/sitemap.json";
+	const outStream = fs.createWriteStream(
+		outputPath,
+		{ flags: "a" } // appends data
+	);
+
+	const xmlSiteMap = fs.readFileSync(dataStream);
+
+	const jsonSiteMap = parser.toJson(xmlSiteMap);
+	const json = JSON.parse(jsonSiteMap);
+	const urls = json.urlset.url;
+
+	// Output as JSON and exit. 
+	await new Promise((resolve, reject) => {
+		outStream.write("[\n");
+		for (i = 0; i < urls.length; i++) {
+			let l = `\t"${urls[i].loc}"${i == urls.length - 1 ? "" : ","}\n`;
+			outStream.write(l);
+		};
+		outStream.write("]\n");
+		outStream.close();
+		outStream.on('close', resolve);
+		outStream.on('error', reject);
+	});
+};
+
+module.exports = convertSitemap;
