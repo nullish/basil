3b01a54f54c4722b0137e5842113b0e7cfe84fad
Nullish <richerde43@gmail.com>
2024-01-21 01:09:35 +0000

    #17 Stream output to local file

--- scripts/getElement.js
+++ scripts/getElement.js
@@ -4,17 +4,20 @@
  */
 
 const puppeteer = require("puppeteer");
+const fs = require('fs');
 const csvOneDimArray = require('../csv-onedim-array'); // Loads CSV input and translates to array, element per row
 const downloadData = require('../downloadData'); // download from HTTP source
 const writeFileAsync = require('../writeFileAsync'); // write file locallyconst 
 const convertSitemap = require('../convertSitemap'); // Converts XML sitemap for JSON input
 
 const basilGetElement = async (args) => {
-  const {parallel, input, urlSitemap, script} = args; // Passed from index.js containing specifics for the scrape
-  const confEl = script.params.find(e => e.key == 'element').value;
-  const confAttr = script.params.find(e => e.key == 'attribute').value;
-  const filePath = "./input/sitemap.xml";
-  const jsonSitemap = "../input/sitemap.json";
+  const { parallel, input, urlSitemap, outputPath, script } = args; // Passed from index.js containing specifics for the scrape
+  const confEl = script.params.find(e => e.key == 'element').value; // Element to search for from config file
+  const confAttr = script.params.find(e => e.key == 'attribute').value; // Attribute to search for from config file
+  const filePath = "./input/sitemap.xml"; // Path to store sitemap XML
+  const jsonSitemap = "../input/sitemap.json"; // Path to store sitemap coverted to JSON
+  const outPath = typeof (outputPath) == 'undefined' ? './output/webscrape.csv' : outputPath;
+  const headerRow = '"timestamp","batch","index","URL","Value","Error"'; // Header row for output
 
   // Get input of URLs from input path or sitemap URL. Input path takes precedence.
   let arrPages;
@@ -24,14 +27,10 @@ const basilGetElement = async (args) => {
     try {
       // Download data from the HTTP resource
       const dataStream = await downloadData(urlSitemap);
-  
-      // Call the function containing fs.createWriteStream
+
       console.log("Downloading sitemap from web")
       await writeFileAsync(filePath, dataStream);
-  
-      // Code here will only execute after the file is written successfully
       console.log('File has been written successfully.');
-      /** @todo Add convertToSitemap and ensure writing completes before attempting to load to array */
       await convertSitemap();
       console.log("Sitemap converted for input to web scrape.")
       arrPages = require(jsonSitemap);
@@ -42,21 +41,27 @@ const basilGetElement = async (args) => {
   const parallelBatches = Math.ceil(arrPages.length / parallel);
 
   console.log(
-    "Scraping " +
-      arrPages.length +
-      " pages for titles, in batches of " +
-      parallel
+    `Scraping ${arrPages.length} pages for titles, in batches of ${parallel}`
   );
 
-  console.log(" This will result in " + parallelBatches + " batches.");
-  console.log('"timestamp","batch","index","URL","Value","Error"');
+  // Remove existing output file if present
+  fs.unlink(outPath, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Existing output deleted.');
+    }
+  });
 
+  console.log(" This will result in " + parallelBatches + " batches.");
+  console.log(headerRow);
+  fs.appendFileSync(outPath, `${headerRow}\n`);
   // Split up the Array of arrPages
   let k = 0;
   for (let i = 0; i < arrPages.length; i += parallel) {
     k++;
     // Launch and Setup Chromium
-    const browser = await puppeteer.launch({headless: "new"});
+    const browser = await puppeteer.launch({ headless: "new" });
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage();
     page.setJavaScriptEnabled(true);
@@ -87,34 +92,33 @@ const basilGetElement = async (args) => {
                 let txtOut;
                 switch (confAttr) {
                   case "innerText":
-                  txtOut = await page.evaluate((el) => el.innerText, elHandle[0]);
-                  txtOut = txtOut.replace(/\n/g, "");
-                  console.log(`"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""`);
-                  break;
+                    txtOut = await page.evaluate((el) => el.innerText, elHandle[0]);
+                    txtOut = txtOut.replace(/\n/g, "");
+                    console.log(`"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""`);
+                    fs.appendFileSync(outPath, `"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""\n`);
+                    break;
                   case "innerHTML":
-                  txtOut = await page.evaluate((el) => el.innerHTML, elHandle[0]);
-                  txtOut = txtOut.replace(/\n/g, "");
-                  console.log(`"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""`);
-                  break;
+                    txtOut = await page.evaluate((el) => el.innerHTML, elHandle[0]);
+                    txtOut = txtOut.replace(/\n/g, "");
+                    console.log(`"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""`);
+                    fs.appendFileSync(outPath, `"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""\n`);
+                    break;
                   default:
-                  txtOut = await page.evaluate((el,a) => el.getAttribute(a), elHandle[0], confAttr);
-                  txtOut = txtOut.replace(/\n/g, "");
-                  console.log(`"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""`);
+                    txtOut = await page.evaluate((el, a) => el.getAttribute(a), elHandle[0], confAttr);
+                    txtOut = txtOut.replace(/\n/g, "");
+                    fs.appendFileSync(outPath, `"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""\n`);
+                    console.log(`"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""`);
                 }
               } else {
                 console.log(
                   `"${timeStamp}","${k}","${j}","${arrPages[elem]}","","ELEMENT NOT FOUND"`
                 );
+                fs.appendFileSync(outPath, `"${timeStamp}","${k}","${j}","${arrPages[elem]}","","ELEMENT NOT FOUND"\n`);
               }
             } catch (err) {
-              throw new Error(err);
-              /*
-              // Report failing element and standard error response
               let timeStamp = new Date(Date.now()).toISOString();
-              console.log(
-                `"${timeStamp}","${k}","${j}","${arrPages[elem]}","","${err}"`
-              );
-              */
+              console.log(`"${timeStamp}","${k}","${j}","${arrPages[elem]}","","${err}"`);
+              fs.appendFileSync(outPath, `"${timeStamp}","${k}","${j}","${arrPages[elem]}","","${err}\n"`);
             }
           }),
         );
