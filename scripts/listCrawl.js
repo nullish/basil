306c0de3b0f6d24370c1be2eb3082988e6344eb3
Nullish <richerde43@gmail.com>
2024-02-04 13:49:05 +0000

    Script working: follow TODO for integration

--- scripts/listCrawl.js
+++ scripts/listCrawl.js
@@ -3,102 +3,64 @@
  * @desc Crawl a list of items such as a search results or product listing and return list of URLs for use in a subsequent web scrape
  */
 
+
+/** @todo Return an array of URLs when completed
+ * Integrate into other scraping scripts as supplier of input
+ */
 const puppeteer = require("puppeteer");
 const fs = require('fs');
 
 const basilListCrawl = async (args) => {
-  const { startUrl, linkSelector, moreItems } = args.listCrawl; // Params needed for crawling list page(s)
-  const outPath = "../input/linkcrawl.csv";
+  const { startUrl, linkSelector, moreItems } = args; // Params needed for crawling list page(s)
 
   console.log(
     `Collecting links to parse from: ${startUrl}`
   );
 
-  // Remove existing output file if present
-  fs.unlink(outPath, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('Existing output deleted.');
-    }
-  });
-
-  // fs.appendFileSync(outPath, `${headerRow}\n`);
-  // Split up the Array of arrPages
-  let k = 0;
-
-  k++;
   // Launch and Setup Chromium
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ['--start-maximized']
+  });
   const context = await browser.createIncognitoBrowserContext();
   const page = await context.newPage();
   page.setJavaScriptEnabled(true);
+  const arrLinks = [];
+  let hasMorePages = true; // Toggle to check whether to scrape a new page of links
 
+  await page.goto(startUrl, {
+    waitUntil: "networkidle2",
+  });
 
-  /** @todo modify to run scrape without using parallel batches */
-  const promises = [];
-  for (let j = 0; j < parallel; j++) {
-    let elem = i + j;
-    // only proceed if there is an element
-    if (arrPages[elem] != undefined) {
-      // Promise to scrape pages
-      // promises push
-      promises.push(
-        browser.newPage().then(async (page) => {
-          try {
-            // Set default navigation timeout.
-            await page.setDefaultNavigationTimeout(30000);
-            // Goto page, wait for timeout as specified in JSON input
-            await page.goto(arrPages[elem], {
-              waitUntil: "networkidle2",
-            });
-            // Get element to search for and report about
-            let elHandle = await page.$x(
-              confEl,
-            );
-            let timeStamp = new Date(Date.now()).toISOString();
-            // Get attribute value to report
-            if (elHandle.length > 0) {
-              let txtOut;
-              switch (confAttr) {
-                case "innerText":
-                  txtOut = await page.evaluate((el) => el.innerText, elHandle[0]);
-                  txtOut = txtOut.replace(/\n/g, "");
-                  console.log(`"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""`);
-                  fs.appendFileSync(outPath, `"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""\n`);
-                  break;
-                case "innerHTML":
-                  txtOut = await page.evaluate((el) => el.innerHTML, elHandle[0]);
-                  txtOut = txtOut.replace(/\n/g, "");
-                  console.log(`"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""`);
-                  fs.appendFileSync(outPath, `"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""\n`);
-                  break;
-                default:
-                  txtOut = await page.evaluate((el, a) => el.getAttribute(a), elHandle[0], confAttr);
-                  txtOut = txtOut.replace(/\n/g, "");
-                  fs.appendFileSync(outPath, `"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""\n`);
-                  console.log(`"${timeStamp}","${k}","${j}","${arrPages[elem]}","${txtOut}",""`);
-              }
-            } else {
-              console.log(
-                `"${timeStamp}","${k}","${j}","${arrPages[elem]}","","ELEMENT NOT FOUND"`
-              );
-              fs.appendFileSync(outPath, `"${timeStamp}","${k}","${j}","${arrPages[elem]}","","ELEMENT NOT FOUND"\n`);
-            }
-          } catch (err) {
-            let timeStamp = new Date(Date.now()).toISOString();
-            console.log(`"${timeStamp}","${k}","${j}","${arrPages[elem]}","","${err}"`);
-            fs.appendFileSync(outPath, `"${timeStamp}","${k}","${j}","${arrPages[elem]}","","${err}\n"`);
-          }
-        }),
-      );
-    }
-  }
+  const scrapeList = async () => {
+    const pageLinks = await page.$$eval(linkSelector, as => as.map(a => a.href));
+    //let arrOut = await pageLinks.map(e => e[0]);
+    arrLinks.push(...pageLinks);
+  };
 
-  // await promise all and close browser
-  await Promise.all(promises);
+  await scrapeList();
+
+  while (hasMorePages) {
+    try {
+      hasMorePages = await page.waitForXPath(moreItems);  
+    } catch (error) {
+      hasMorePages = false;
+    };
+    if (hasMorePages) {
+      await hasMorePages.click();
+      await page.waitForNetworkIdle({idleTime: 1000});
+      await scrapeList();
+    };
+  };
+
+  console.log(arrLinks);
   await browser.close();
-
 };
 
-module.exports = basilListCrawl;
+//module.exports = basilListCrawl;
+basilListCrawl({
+  startUrl: "https://www.shu.ac.uk/courses?page=100&perPage=5&query=&yearOfEntry=2024%2F25",
+  linkSelector: "::-p-xpath(//a[@class='m-snippet__link'])",
+  moreItems: "(//button[contains(@aria-label, 'Go to page') and .//span[contains(@class, 'chevron--right')]])[1]",
+});
