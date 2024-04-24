da1e66006c68e8d147e85f217d46726dd73264d0
Nullish <richerde43@gmail.com>
2024-04-24 16:38:43 +0100

    Set up to write all resources by page to file

--- scripts/resources.js
+++ scripts/resources.js
@@ -1,101 +1,68 @@
 /**
  * @name Resources
  *
- * @desc Returns resources downloaded by page
+ * @desc Get details of all HTTP requested resources from a user provided list of pages.
  */
-const puppeteer = require("puppeteer");
-const fs = require("fs");
 
-const basilResources = async (args) => {
+const puppeteer = require("puppeteer");
+const fs = require('fs');
+
+ const basilResources = async (args) => {
   const {parallel, outputPath, arrUniquePages} = args; // Passed from index.js containing specifics for the scrape
-  const outPath =
-    typeof outputPath == "undefined" ? "./output/webscrape.csv" : outputPath;
-  const headerRow =
-    '"timeStamp","RequestURL","resourceDomain","Error"'; // Header row for output
+  const outPath = typeof (outputPath) == 'undefined' ? './output/webscrape.csv' : outputPath;
+  const headerRow = '"timestamp","URL","resourceURI","Error"'; // Header row for output
 
   const parallelBatches = Math.ceil(arrUniquePages.length / parallel);
-  console.log(
-    `Scraping ${arrUniquePages.length} pages for resource domains, in batches of ${parallel}`
-  );
 
-  console.log(" This will result in " + parallelBatches + " batches.");
+  console.log('Scraping ' + arrUniquePages.length + ' pages in batches of ' + parallel);
+
+  console.log(' This will result in ' + parallelBatches + ' batches.');
   console.log(headerRow);
-
   fs.appendFileSync(outPath, `${headerRow}\n`);
-  // Split up the Array of arrPages
-  let k = 0;
+
+  // Split up the Array of arrUniquePages
+  let k = 0
   for (let i = 0; i < arrUniquePages.length; i += parallel) {
     k++;
     // Launch and Setup Chromium
     const browser = await puppeteer.launch({headless: "new"});
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage();
-    page.setJavaScriptEnabled(true);
+    page.setJavaScriptEnabled(true)
 
-    const promises = [];
+    const promises = []
     for (let j = 0; j < parallel; j++) {
-      let elem = i + j;
-      // only proceed if there is an element
+      let elem = i + j
+      // only proceed if there is an element 
       if (arrUniquePages[elem] != undefined) {
         // Promise to scrape pages
         // promises push
-        promises.push(
-          browser.newPage().then(async (page) => {
-            try {
-              // Set default navigation timeout.
-              await page.setDefaultNavigationTimeout(30000);
-              let ress = [];
-              // Goto page, wait for timeout as specified in JSON input
-              page.on('requestfinished', (request) => {
-                ress.push(request.url());
+        promises.push(browser.newPage().then(async page => {
+          try {
+            // Set default navigation timeout.
+            await page.setDefaultNavigationTimeout(30000); 
+            // Build array of all resources downloaded in request for output
+            let ress = [];
+            page.on('requestfinished', (request) => {
+              ress.push(request.url());
+          });
+            await page.goto(arrUniquePages[elem], {
+              waitUntil: "networkidle2",
             });
-              let res = await page.goto(arrUniquePages[elem], {
-                waitUntil: "networkidle2",
-              });
-              let timeStamp = new Date(Date.now()).toISOString();
-              let arrOut;
-              if (res.request().redirectChain().length > 0) {
-                let chain = res.request().redirectChain();
-                let lastRedirect = chain[chain.length - 1];
-                let lastRedirectStatusCode = lastRedirect._response._status;
-                let lastRedirectStatusText = lastRedirect._response._statusText;
-                arrOut = [
-                  timeStamp,
-                  arrUniquePages[elem],
-                  resUrl,
-                  stCode,
-                  stText,
-                  lastRedirectStatusCode,
-                  lastRedirectStatusText,
-                  pageTitle,
-                  heading,
-                ];
-              } else {
-                arrOut = [
-                  timeStamp,
-                  arrUniquePages[elem],
-                  resUrl,
-                  stCode,
-                  stText,
-                  "",
-                  "",
-                  pageTitle,
-                  heading,
-                ];
-              }
-              let strOut = arrOut.join('","');
-              fs.appendFileSync(outPath, `"${strOut}"\n`);
-              console.log(`"${strOut}"`);
-            } catch (err) {
-              // Report failing element and standard error response
-              let timeStamp = new Date(Date.now()).toISOString();
-              fs.appendFileSync(outPath, `"${timeStamp}","${arrUniquePages[elem]}","","","","","","","",""${err}\n"`);
-              console.log(
-                `"${timeStamp}","${arrUniquePages[elem]}","","","","","","","",""${err}"`
-              );
-            }
-          })
-        );
+            let timeStamp = new Date(Date.now()).toISOString(); 
+            let arrOut = await ress.map(e => [timeStamp, arrUniquePages[elem], e]);
+            let strOut = arrOut.map(e => ('"' + e.join('","') + '",""'));
+            strOut.forEach(e => {
+              console.log(e);
+              fs.appendFileSync(outPath, `${e}\n`);
+            });
+          } catch (err) {
+            // Report failing element and standard error response
+            let timeStamp = new Date(Date.now()).toISOString();
+            console.log(`"${timeStamp}","${arrUniquePages[elem]}","","${err}"`);
+            fs.appendFileSync(outPath, `"${timeStamp}","${arrUniquePages[elem]}","","${err}"`);
+          }
+        }));
       }
     }
 
